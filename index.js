21d8d6eb4c89e0a9c155d6df7fe85bfbc40a7ae4
yuta0801 <yuta20030801@icloud.com>
2017-10-05 18:43:12 +0900

    Use EventEmitter
    - Extend EventEmitter
    - Change linebreak-style

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const Util = require('../../Util')
+const {EventEmitter} = require('events')
 const setting = require('./settings')
 const BouyomiChan = require('./software/BouyomiChan')
 const Softalk = require('./software/Softalk')
 
-class Index {
+class Index extends EventEmitter {
   constructor() {
+    super()
     switch (setting.software) {
-    case 'BouyomiChan': this.software = new BouyomiChan(); break
-    case 'Softalk':     this.software = new Softalk();     break
-    default: Util.showError('ソフトが正しくありません')
+      case 'BouyomiChan': this.software = new BouyomiChan(); break
+      case 'Softalk':     this.software = new Softalk();     break
+      default: Util.showError('ソフトが正しくありません')
     }
-  }
-
-  message(item) {
-    this.software.message(item.message)
+    this.on('message', data => {
+      this.software.message(data.message)
+    })
   }
 }
 
