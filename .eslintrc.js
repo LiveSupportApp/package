6b7ae7ca0fbb0c3a3a9b5c921a0cca63abf6dfe3
yuta0801 <yuta20030801@icloud.com>
2017-10-05 18:45:06 +0900

    Add .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,27 @@
-D:/programming/projects/LiveSupport/.eslintrc.js
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [2, 2, {'SwitchCase': 1}],
+        'linebreak-style': ['error', 'windows'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'never'],
+        'no-console': 'warn',
+        'comma-dangle': ['error', 'always-multiline'],
+        'eqeqeq': 'error',
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'comma-style': ['error', 'last'],
+        'no-multiple-empty-lines': [1, { 'max': 1 }],
+        'no-spaced-func': 'error',
+        'keyword-spacing': 'error',
+        'space-before-blocks': 'error',
+    },
+}
